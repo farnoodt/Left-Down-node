cd1c1dd6d3eb3643f57e5e732a93d4ffd991d9eb
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-10-13 23:24:18 -0700

    Add project files.

---
+++ Left-Down-node/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Left_Down_node
+{
+    class Program
+    {
+        private static int L = 0;
+        private static int res = 0;
+        static void Main(string[] args)
+        {
+            TreeNode tree = new TreeNode(1);
+            tree.left = new TreeNode(2);
+            tree.right = new TreeNode(3);
+            tree.left.left = new TreeNode(4);
+            tree.right.left = new TreeNode(5);
+            tree.right.left.left = new TreeNode(7);
+            tree.right.right = new TreeNode(6);
+            Console.WriteLine(FindBottomLeftValue(tree));
+        }
+
+        public static int FindBottomLeftValue(TreeNode root)
+        {
+            FindBottomLeftValueUtil(root, 1);
+            return res;
+        }
+
+        public static void FindBottomLeftValueUtil(TreeNode root,int Curr)
+        {
+            //if (root == null)
+            //    return ;
+            if ( Curr>L)
+            {
+                L=Curr ;
+                res = root.data;
+            }
+
+            if (root.left != null)
+                FindBottomLeftValueUtil(root.left, Curr + 1);
+            //L--;
+            if (root.right != null)
+                FindBottomLeftValueUtil(root.right, Curr + 1);
+
+
+        }
+    }
+}
---
+++ Left-Down-node/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_Down_node
+{
+    
+    public class TreeNode
+    {
+        public int data;
+        public TreeNode left, right;
+        public TreeNode(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
